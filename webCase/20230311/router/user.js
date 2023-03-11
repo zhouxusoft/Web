8e46629ab3db3190b37df7f44f787440ca7f3ebe
周旭 <zhouxusoft@gmail.com>
2023-03-11 18:42:46 +0800

    简单的前后端登陆注册demo

---
+++ webCase/20230311/router/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+//创建路由对象
+const router = express.Router()
+
+//导入用户路由处理函数对应的模块
+const user_handle = require('../router_handle/user')
+
+//注册新用户
+router.post('/reguser', user_handle.regUser)
+
+//登录
+router.post('/login', user_handle.login)
+
+//将路由对象共享出去
+module.exports = router
