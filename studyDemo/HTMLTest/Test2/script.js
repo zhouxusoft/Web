235f350f0b1c050dc47cae0758fa5e9b37af866d
周旭 <zhouxusoft@gmail.com>
2024-05-28 10:37:41 +0800

    ..

--- studyDemo/HTMLTest/Test2/script.js
+++ studyDemo/HTMLTest/Test2/script.js
@@ -11,26 +11,27 @@ document.addEventListener('scroll', function () {
     }
 })
 
-var gradientBackground = document.getElementById('gradient-background');
+var gradientBackground = document.getElementById('gradient-background')
 
 gradientBackground.addEventListener('mousemove', function (e) {
-    var x = e.clientX; // 获取鼠标位置的X坐标
-    var y = e.clientY; // 获取鼠标位置的Y坐标
+    var x = e.clientX // 获取鼠标位置的X坐标
+    var y = e.clientY // 获取鼠标位置的Y坐标
 
-    var rect = gradientBackground.getBoundingClientRect();
+    var rect = gradientBackground.getBoundingClientRect()
 
-    var localX = x - rect.left;
-    var localY = y - rect.top;
+    var localX = x - rect.left
+    var localY = y - rect.top
 
-    var width = gradientBackground.offsetWidth; // 获取元素宽度
-    var height = gradientBackground.offsetHeight; // 获取元素高度
+    var width = gradientBackground.offsetWidth // 获取元素宽度
+    var height = gradientBackground.offsetHeight // 获取元素高度
 
     // 计算鼠标位置占元素的百分比
-    var xPercent = localX / width * 100;
-    var yPercent = localY / height * 100;
+    var xPercent = localX / width * 100
+    var yPercent = localY / height * 100
 
     // 更新渐变背景的中心点
-    gradientBackground.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, #dfedf4 0, #628699 20%, #628699 100%)`;
-    gradientBackground.style.webkitBackgroundClip = 'text'; // 对于支持的 WebKit 浏览器
-    gradientBackground.style.backgroundClip = 'text';
-});
+    gradientBackground.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, #dfedf4 0, #628699 20%, #628699 100%)`
+    gradientBackground.style.backgroundClip = 'text'
+})
+
+const rotatetextbox = document.getElementsByClassName('rotatetextbox')[0]
