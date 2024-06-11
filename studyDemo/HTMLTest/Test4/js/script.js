8041f03b20adad1998905276dee90eed5b677284
周旭 <zhouxusoft@gmail.com>
2024-06-12 00:59:01 +0800

    ..

--- studyDemo/HTMLTest/Test4/js/script.js
+++ studyDemo/HTMLTest/Test4/js/script.js
@@ -1,4 +1,5 @@
 let citycode = 0
+let cityname = ''
 let cityweatherinfo = {}
 
 const weathericon = $(".weathericon").eq(0)
@@ -11,19 +12,22 @@ const feelsLikeinfo = $(".feelsLikeinfo").eq(0)
 const visinfo = $(".visinfo").eq(0)
 const precipinfo = $(".precipinfo").eq(0)
 const pressureinfo = $(".pressureinfo").eq(0)
+const placeandtimetime = $(".placeandtimetime").eq(0)
+const placetext = $(".placetext").eq(0)
 
 $(".gologobox").eq(0).click(function () {
     let searchkey = $('#searchcity').val()
     if (searchkey.trim() === '') {
         alert('输入不能为空！')
     } else {
+        $('#searchcity').val('')
         $.ajax({
             url: `https://geoapi.qweather.com/v2/city/lookup?location=${searchkey}&key=c009167ad03e4cd781a7a3e5dd8e3357`,
             method: 'GET',
             success: function (data) {
-                console.log(data.location[0].id)
                 if (data.code == 200) {
                     if (data.location.length > 0) {
+                        cityname = data.location[0].name
                         citycode = data.location[0].id
                         $.ajax({
                             url: `https://devapi.qweather.com/v7/weather/now?location=${citycode}&key=13ea0f93c16c401db51b5130dccee250`,
@@ -53,6 +57,8 @@ $(".gologobox").eq(0).click(function () {
 })
 
 function updateWeatherInfoBox() {
+    let date = new Date(cityweatherinfo.obsTime)
+    let formattedDate = date.toLocaleString()
     weathericon.attr('src', `./static/icons/${cityweatherinfo.icon}.svg`)
     currentweathernum.text(`${cityweatherinfo.temp}°`)
     currentweathertext.text(`${cityweatherinfo.text}`)
@@ -63,6 +69,8 @@ function updateWeatherInfoBox() {
     visinfo.text(`${cityweatherinfo.vis}km`)
     precipinfo.text(`${cityweatherinfo.precip}mm`)
     pressureinfo.text(`${cityweatherinfo.pressure}hPa`)
+    placeandtimetime.text(`${formattedDate}`)
+    placetext.text(`${cityname}`)
     $('.currentweatherbox').css('display', 'flex')
     $('.currentweatherlist').css('display', 'flex')
 }
