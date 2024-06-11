eaae2c523c69547a1ddc3f12cff22595c3171ba1
周旭 <zhouxusoft@gmail.com>
2024-06-12 05:25:08 +0800

    基本完成

--- studyDemo/HTMLTest/Test4/js/script.js
+++ studyDemo/HTMLTest/Test4/js/script.js
@@ -1,6 +1,10 @@
 let citycode = 0
 let cityname = ''
 let cityweatherinfo = {}
+let sevendayweatherinfo = {}
+let perhourweatherinfo = {}
+
+const daysOfWeek = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
 
 const weathericon = $(".weathericon").eq(0)
 const currentweathernum = $(".currentweathernum").eq(0)
@@ -14,6 +18,16 @@ const precipinfo = $(".precipinfo").eq(0)
 const pressureinfo = $(".pressureinfo").eq(0)
 const placeandtimetime = $(".placeandtimetime").eq(0)
 const placetext = $(".placetext").eq(0)
+const dayweatherdayxinqi = $(".dayweatherdayxinqi")
+const dayweatherdayriqi = $(".dayweatherdayriqi")
+const dayweatherlogo = $(".dayweatherlogo")
+const maxweathernum = $(".maxweathernum")
+const minweathernum = $(".minweathernum")
+const perhourweathertemp = $(".perhourweathertemp")
+const perhourweathertempheight = $(".perhourweathertempheight")
+const perhourweathericon = $(".perhourweathericon")
+const perhourweatherdir = $(".perhourweatherdir")
+const perhourweathertime = $(".perhourweathertime")
 
 $(".gologobox").eq(0).click(function () {
     let searchkey = $('#searchcity').val()
@@ -30,13 +44,43 @@ $(".gologobox").eq(0).click(function () {
                         cityname = data.location[0].name
                         citycode = data.location[0].id
                         $.ajax({
-                            url: `https://devapi.qweather.com/v7/weather/now?location=${citycode}&key=13ea0f93c16c401db51b5130dccee250`,
+                            url: `https://devapi.qweather.com/v7/weather/now?location=${citycode}&key=c009167ad03e4cd781a7a3e5dd8e3357`,
                             method: 'GET',
                             success: function (data) {
                                 console.log(data)
                                 if (data.code == 200) {
                                     cityweatherinfo = data.now
                                     updateWeatherInfoBox()
+                                }
+                            },
+                            error: function () {
+                                alert('请求失败')
+                            }
+                        })
+
+                        $.ajax({
+                            url: `https://devapi.qweather.com/v7/weather/7d?location=${citycode}&key=c009167ad03e4cd781a7a3e5dd8e3357`,
+                            method: 'GET',
+                            success: function (data) {
+                                console.log(data)
+                                if (data.code == 200) {
+                                    sevendayweatherinfo = data.daily
+                                    updateSevenDayWeather()
+                                }
+                            },
+                            error: function () {
+                                alert('请求失败')
+                            }
+                        })
+
+                        $.ajax({
+                            url: `https://devapi.qweather.com/v7/weather/24h?location=${citycode}&key=c009167ad03e4cd781a7a3e5dd8e3357`,
+                            method: 'GET',
+                            success: function (data) {
+                                console.log(data)
+                                if (data.code == 200) {
+                                    perhourweatherinfo = data.hourly
+                                    updatePerHourWeather()
                                 }
                             },
                             error: function () {
@@ -56,6 +100,9 @@ $(".gologobox").eq(0).click(function () {
     }
 })
 
+$('#searchcity').val('南昌')
+$(".gologobox").eq(0).click()
+
 function updateWeatherInfoBox() {
     let date = new Date(cityweatherinfo.obsTime)
     let formattedDate = date.toLocaleString()
@@ -73,4 +120,56 @@ function updateWeatherInfoBox() {
     placetext.text(`${cityname}`)
     $('.currentweatherbox').css('display', 'flex')
     $('.currentweatherlist').css('display', 'flex')
+}
+
+function updateSevenDayWeather() {
+    let currentDate = new Date()
+    let dayOfWeek = currentDate.getDay()
+    let dayName = '今天'
+
+    for (let i = 0; i < 7; i++) {
+        if (i != 0) {
+            dayName = daysOfWeek[(dayOfWeek + i) % 7]
+        }
+        let day = sevendayweatherinfo[i].fxDate
+        const date = new Date(day)
+        const formattedDate = new Intl.DateTimeFormat('zh-CN', {
+            month: 'long',
+            day: 'numeric'
+        }).format(date)
+        dayweatherdayxinqi.eq(i).text(`${dayName}`)
+        dayweatherdayriqi.eq(i).text(`${formattedDate}`)
+        dayweatherlogo.eq(i).attr('src', `./static/icons/${sevendayweatherinfo[i].iconDay}.svg`)
+        maxweathernum.eq(i).text(`${sevendayweatherinfo[i].tempMax}°`)
+        minweathernum.eq(i).text(`${sevendayweatherinfo[i].tempMin}°`)
+    }
+}
+
+function updatePerHourWeather() {
+    let mintemp = 99
+    let maxtemp = -99
+    for (let i = 0; i < 24; i = i + 2) {
+        if (perhourweatherinfo[i].temp > maxtemp) {
+            maxtemp = perhourweatherinfo[i].temp
+        }
+        if (perhourweatherinfo[i].temp < mintemp) {
+            mintemp = perhourweatherinfo[i].temp
+        }
+    }
+
+    let tempheight = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
+
+    for (let i = 0; i < 12; i++) {
+        tempheight[i] = 20 + 60 * (perhourweatherinfo[2 * i].temp - mintemp) / (maxtemp - mintemp)
+        tempheight[i] = Math.floor(tempheight[i])
+    }
+    // console.log(tempheight)
+    
+    for (let i = 0; i < 12; i++) {
+        perhourweathertemp.eq(i).text(`${perhourweatherinfo[2 * i].temp}°`)
+        perhourweathertempheight.eq(i).css('height', `${tempheight[i]}px`)
+        perhourweathericon.eq(i).attr('src', `./static/icons/${perhourweatherinfo[2 * i].icon}.svg`)
+        perhourweatherdir.eq(i).css('rotate', `${perhourweatherinfo[2 * i].wind360}deg`)
+        perhourweathertime.eq(i).text(`${perhourweatherinfo[2 * i].fxTime.substring(11, 16)}`)
+    }
 }
